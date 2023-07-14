f14a62a439629c899a8980ac02b8bee86169f0f6
Mrsuper125 <Mrsuper1221@gmail.com>
2023-07-14 13:43:11 +0300

    API endpoints changes

--- src/shared/api.ts
+++ src/shared/api.ts
@@ -2,13 +2,13 @@ import axios from "axios";
 import {Link} from "./link.ts";
 
 export async function getUrl(id : number){
-    const response = await axios.get(`http://37.220.80.103:8000/${id}`, {headers:{'Access-Control-Allow-Origin' : '*'}})
+    const response = await axios.get(`http://localhost:8000/${id}`, {headers:{'Access-Control-Allow-Origin' : '*'}})
     const result : Link = {url: response.data.url, expirationDate: response.data.expireDate}
     return result
 }
 
 export async function postUrl(link : Link){
     // @ts-ignore
-    const response = await axios.post("http://37.220.80.103:8000/add", {url:link.url, expiration_date: link.expirationDate}, {headers:{'Access-Control-Allow-Origin' : '*'}})
+    const response = await axios.post("http://localhost:8000/add", {url:link.url, expiration_date: link.expirationDate}, {headers:{'Access-Control-Allow-Origin' : '*'}})
     return response.data
 }
