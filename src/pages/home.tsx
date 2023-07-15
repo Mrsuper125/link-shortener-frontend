f03a9b41abb7c0a36e4c8b7c142f1c517eec65d7
Mrsuper125 <Mrsuper1221@gmail.com>
2023-07-15 15:31:28 +0300

    starter Mantine migration

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,6 +2,8 @@ import React, {useEffect, useState} from 'react';
 import "../shared/styles.css"
 import validator from 'validator'
 import {postUrl} from "../shared/api.ts";
+import {MantineProvider, Radio, Button, TextInput} from "@mantine/core";
+import {useStyles} from "../shared/styles.ts";
 
 let selected : string, url : string, comment : string;
 let setSelected : React.Dispatch<string>, setUrl : React.Dispatch<string>, setComment : React.Dispatch<string>;
@@ -29,58 +31,50 @@ function tryPosting(){
 }
 
 const Home = () => {
-    [selected, setSelected] = useState('hour');
+    [selected, setSelected] = useState("hour");
     [url, setUrl] = useState("");
     [comment, setComment] = useState("");
     [valid, setValid] = useState(false);
+    const { classes} = useStyles();
     useEffect(() => {
         if (url == ""){
             setComment("");
             setValid(false)
         }
         else {
-            if (validator.isURL(url)){
+            if (validator.isURL(url)) {
                 setComment("")
                 setValid(true)
-            }
-            else {
+            } else {
                 setComment("Ваша ссылка неправильная")
                 setValid(false)
             }
         }
     }, [url])
     return (
-        <form className={"create-link-form"} onSubmit={(e) => e.preventDefault()}>
-            <h1 className={"header"}>Сокращатель ссылок</h1>
-            <hr/>
-            <div className={"form-items"}>
-                <h2 className={"instruction"}> Введите адрес ссылки:</h2>
-                <div className={"field-with-comment"}>
-                    <input type={"url"} placeholder={"Ваша ссылка"} className={"field"} onChange={(e) => setUrl(e.target.value)}/>
-                    <p className={"comment"} id={"comment"}>{comment}</p>
-                </div>
-                <div>
-                    <button className={"create-button"} onClick={() => tryPosting()}>Создать</button>
-                </div>
-                <h3 className={"instruction"}>Выберите срок действия ссылки:</h3>
-                <div className={"radio-group"}>
-                    <div>
-                        <input type={"radio"} className={"radio"} name={"expireDate"} id = {"hour"} checked={selected === "hour"} onChange={(event)=>setSelected(event.target.id)}/>
-                        <label className={"radio-label"}>1 час</label>
+        <MantineProvider>
+            <form className={"create-link-form"} onSubmit={(e) => e.preventDefault()}>
+                <h1 className={"header"}>Сокращатель ссылок</h1>
+                <hr/>
+                <div className={"form-items"}>
+                    <h2 className={"instruction"}> Введите адрес ссылки:</h2>
+                    <div className={"field-with-comment"}>
+                        <TextInput type={"url"} placeholder={"Ваша ссылка"} className={"field"} error={comment} onChange={(e) => setUrl(e.target.value)}/>
                     </div>
                     <div>
-                        <input type={"radio"} className={"radio"} name={"expireDate"} id = {"day"} checked={selected === "day"} onChange={(event)=>setSelected(event.target.id)}/>
-                        <label className={"radio-label"}>1 сутки</label>
+                        <Button variant={"outline"} className={"create-button"} color={"dark"} onClick={() => tryPosting()}>Создать</Button>
                     </div>
-                    <div>
-                        <input type={"radio"} className={"radio"} name={"expireDate"} id = {"week"} checked={selected === "week"} onChange={(event)=>setSelected(event.target.id)}/>
-                        <label className={"radio-label"}>1 неделя</label>
-                    </div>
+                    <h3 className={"instruction"}>Выберите срок действия ссылки:</h3>
+                    <Radio.Group value={selected} onChange={setSelected}>
+                        <Radio classNames={{label: classes.radioLabel}} className="radio" label = {"1 час"} value="hour" onChange={(event)=>setSelected(event.target.value)}/>
+                        <Radio classNames={{label: classes.radioLabel}} className="radio" label = {"1 день"} value="day" onChange={(event)=>setSelected(event.target.value)}/>
+                        <Radio classNames={{label: classes.radioLabel}} className="radio" label = {"1 неделя"} value="week" onChange={(event)=>setSelected(event.target.value)}/>
+                    </Radio.Group>
                 </div>
-            </div>
-            <hr/>
-            <p className={"credits"}>Сделано с любовью Александром Маратовичем</p>
-        </form>
+                <hr/>
+                <p className={"credits"}>Сделано с любовью Александром Маратовичем</p>
+            </form>
+        </MantineProvider>
     );
 };
 
---
+++ src/shared/styles.ts
@@ -0,0 +1,10 @@
+import {createStyles, px} from '@mantine/core';
+
+// @ts-ignore
+export const useStyles = createStyles((theme) => ({
+    radioLabel: {
+        verticalAlign: "center",
+        display: "inline-block",
+        paddingLeft: px(5),
+    },
+}));
